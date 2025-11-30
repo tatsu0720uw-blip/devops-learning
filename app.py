e0f1c279aaec319924834c35ab6c0bbb7cc0acff
tatsu0720uw-blip <tatsu0720.uw@gmail.com>
2025-11-30 20:37:46 +0900

    fix: del comment

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask
 
 
-app = Flask(__name__)  # インラインコメントの前にスペースを2つ
+app = Flask(__name__)
 
 
-@app.route('/') # 関数定義の前に2行の空白行
+@app.route('/')
 def hello():
     return "Hello, DevOps World! This app is running inside a Docker container."
 
 
-if __name__ == '__main__': # インラインコメントの前にスペースを2つ
+if __name__ == '__main__':
     # 開発環境で動作確認するための設定
     app.run(host='0.0.0.0', port=5000)
-# (ファイルの最後に必ず空行（改行）を1つ入れる)
